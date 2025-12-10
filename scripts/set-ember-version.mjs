92b690d050e4958eba59dd8a1327fea73869a995
Edward Faulkner <edward@eaf4.com>
2025-12-09 21:32:20 -0500

    try scenarios

--- scripts/set-ember-version.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'node:fs';
-let pkg = JSON.parse(fs.readFileSync('package.json'));
-pkg.devDependencies['ember-source'] = process.argv[2];
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
